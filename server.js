4e28d590827772040addf52dc53ccd5451324dbc
mgwdev81 <mgwalters@Matthews-MacBook.local>
2016-03-26 16:07:38 +1100

    Added server.js entry point and npm dependencies.

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
