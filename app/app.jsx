f33e2d95390c7e64720af42fb871ca3bcefc3d8e
mgwdev81 <mgwalters@Matthews-MacBook.local>
2016-03-26 20:10:07 +1100

    Added a NoticeBox component for notice submit functionality.

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,27 @@
-var noticeboardService = {
-    postNotice: function(noticeBody) {
-        var notice = { body: noticeBody }
+var NoticeBox = React.createClass({displayName: 'NoticeBox',
+  getInitialState: function() {
+    return {value: ''};
+  },
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+  },
+  postNotice: function() {
+        var notice = { body: this.state.value }
         $.post('http://localhost:3000/api/notice',
             notice,
             function() { console.log('Success'); });
-    }
-}
-
-noticeboardService.postNotice("This is a test notice from app.jsx.");
+  },
+  render: function() {
+      return (
+          <div>
+              <textarea
+                  value={this.state.value}
+                  onChange={this.handleChange}/>
+              <button onClick={this.postNotice}>Submit</button>
+          </div>);
+  }
+});
+ReactDOM.render(
+    React.createElement(NoticeBox, null),
+    document.getElementById('content')
+);
