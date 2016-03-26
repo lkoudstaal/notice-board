cd33b0398a949ed70e299c52cbb40cb0c60b2299
mgwdev81 <mgwalters@Matthews-MacBook.local>
2016-03-26 17:21:25 +1100

    Added a test insertDocument method to test MongoDB database insert.

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
+var assert = require('assert');
 var express = require('express');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var url = 'mongodb://localhost:27017/notice-board';
+
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+var insertDocument = function(db, callback) {
+    db.collection('notices').insertOne({
+        "body" : "A notice body."
+    }, function(err, result) {
+        assert.equal(err, null);
+        console.log("Inserted a notice board item into the notices collection.");
+        callback();
+    });
+};
+
+MongoClient.connect(url, function(err, db) {
+    assert.equal(err, null);
+    insertDocument(db, function() {
+        db.close();
+    });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
