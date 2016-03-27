da7441924160574094ed654ccce479887b5f6368
mgwdev81 <mgwdev81@gmail.com>
2016-03-28 00:42:47 +1100

    Moved database connection out of server.js into database.js.

---
+++ data/database.js
@@ -0,0 +1,29 @@
+(function(database) {
+
+    var mongodb = require("mongodb");
+    var mongoUrl = 'mongodb://localhost:27017/notice-board';
+    var theDb = null;
+
+    // Defer connection to database until the first time it is required.
+    // Keep database connection open and return the same database 
+    // connection to subsequent callers to enable connection pooling 
+    // optimisations.
+    database.getDb = function(next) {
+        if (theDb) {
+            next(null, db);
+        } else {
+            mongodb.MongoClient.connect(mongoUrl, function(err, db) {
+                if (err) {
+                    next(err, null);
+                } else {
+                    theDb = { 
+                        db: db,
+                        notices: db.collection("notices")
+                    };
+                    next(null, theDb);
+                }
+            });
+        }
+    }
+
+})(module.exports);
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var assert = require('assert');
 var express = require('express');
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
-var url = 'mongodb://localhost:27017/notice-board';
-
+var database = require("./data/database.js")
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
@@ -14,26 +11,27 @@ app.use('/', express.static('app'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var insertDocument = function(notice, db, callback) {
-    db.collection('notices').insertOne(
-        notice, function(err, result) {
-            assert.equal(err, null);
-            console.log("Inserted a notice board item into the notices collection.");
-            callback();
-        });
+var insertNotice = function(notice, db, callback) {
+    db.getDb(function(err, db) {
+        db.notices.insertOne(
+            notice,
+            function(err, result) {
+                assert.equal(err, null);
+                console.log("Inserted a notice item.");
+                callback();
+            });
+    });
 };
 
 app.post('/api/notice', function(req, res) {
-    // TODO: Check that req.body is a valid notice. 
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(err, null);
-        insertDocument(req.body, db, function() {
-            db.close();
-            res.status(200).send("OK");
-        });
+    // TODO: Check that req.body is a valid notice.
+    insertNotice(req.body, database, function() {
+        res.status(200).send("OK");
     });
 });
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
+
+// TODO: Close database connection on server shutdown.
