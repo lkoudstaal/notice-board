3ce63021f4eccd6c4102ce2e3c521aea1462b728
mgwdev81 <mgwdev81@gmail.com>
2016-04-06 23:44:59 +1000

    Using Bootstrap instead of custom stylesheet. Also now using Bower for client-side packages.

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use('/', express.static('app'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Set the public static resource folder
+app.use(express.static(__dirname + "/public"));
+
 // Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
     // Set permissive CORS header - this allows this server to be used only as
