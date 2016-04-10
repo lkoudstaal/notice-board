6cb1ccecf7f84a3541b4e1e6a6f1993c23d0845f
mgwdev81 <mgwdev81@gmail.com>
2016-04-10 22:05:57 +1000

    Replaced calls to the deprecated function Gulp.Run and simplified gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,11 @@ var gulp = require('gulp');
 var spawn = require('child_process').spawn;
 var node;
 
-// Launches the server and restarts the server when particular
-// source files are changed.
-gulp.task('default', function() {
-        // TODO: gulp.run() has been deprecated. Use task dependencies or gulp.watch task triggering instead.
-        gulp.run('server');
+gulp.task('default', ['startServer']);
 
-        gulp.watch(['./server.js', './app/*.jsx', './data/*.js', './controllers/*.js'], function() {
-        // TODO: As above.
-        gulp.run('server');
-    });
-});
-
-// Launches the server.
-// If a server is already running it is killed.
-gulp.task('server', function() {
-        if (node) node.kill();
-        node = spawn('node', ['server.js'], {stdio: 'inherit'});
-        node.on('close', function (code) {
-        if (code === 8) {
-            gulp.log('Error detected, waiting for changes...');
-        }
-    });
-});
-
-// Clean up if an error goes unhandled.
-process.on('exit', function() {
+gulp.task('startServer', function() {
     if (node) node.kill();
+    node = spawn('node', ['server.js']);
 });
+
+var watcher = gulp.watch(['server.js', 'app/*.jsx', 'data/*.js', 'controllers/*.js'], ['startServer']);
