e6dc7fa25dc1d17b8d01f4e1384d78cb730c6f87
mgwdev81 <mgwdev81@gmail.com>
2016-04-14 23:05:10 +1000

    Gulp now also starts mongod. stdout and stderr is now logged to the console.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
+// TODO: Gracefully close down mongod and node child processes when user ctrl-c's.
+
 var gulp = require('gulp');
 var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var node;
+var mongod;
 
-gulp.task('default', ['startServer']);
+gulp.task('default', ['startDatabaseServer', 'startNodeServer']);
 
-gulp.task('startServer', function() {
+gulp.task('startDatabaseServer', function() {
+    mongod = spawn('mongod', ['--config', '/usr/local/etc/mongod.conf']);
+    mongod.stdout.on('data', function(data) {
+        console.log('mongod.stdout: ' + data);
+    });
+    mongod.stderr.on('data', function(data) {
+        console.log('mongod.stderr: ' + data);
+    });
+})
+
+gulp.task('startNodeServer', function() {
     if (node) node.kill();
     node = spawn('node', ['server.js']);
+    node.stdout.on('data', function(data) {
+        console.log('node.stdout: ' + data);
+    });
+    node.stderr.on('data', function(data) {
+        console.log('node.stderr: ' + data);
+    });
 });
 
 var watcher = gulp.watch(['server.js', 'app/*.jsx', 'data/*.js', 'controllers/*.js'], ['startServer']);
