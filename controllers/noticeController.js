14f983d37d6e46bd00b1cba4fe6a6db330453981
mgwdev81 <mgwdev81@gmail.com>
2016-04-15 17:29:22 +1000

    Moved insertNotice and findNotice data related functions out of controller and into database class.

--- controllers/noticeController.js
+++ controllers/noticeController.js
@@ -1,9 +1,5 @@
 (function (noticeController) {
 
-    var assert = require('assert');
-    var dateFormat = require('dateformat');
-    var mongodb = require('mongodb');
-
     noticeController.init = function (app, database) {
 
         app.get('/', function (req, res) {
@@ -12,44 +8,18 @@
 
         app.post('/api/notice', function (req, res) {
             // TODO: Check that req.body is a valid notice.
-            insertNotice(req.body, database, function () {
+            // TODO: Handle error when insertNotice fails.
+            database.insertNotice(req.body, function () {
                 res.status(200).send("OK");
             });
         });
 
         app.get('/api/notices', function (req, res) {
-            findNotices(database, function (results) {
+            // TODO: Handle error when findNotices fails.
+            database.findNotices(function (results) {
                 res.status(200).send(results);
-            })
+            });
         });
-
-        var insertNotice = function (notice, database, callback) {
-            database.getDb(function (err, db) {
-                // TODO: Handle case where getDb errors.
-                db.notices.insertOne(
-                    notice,
-                    function (err, result) {
-                        assert.equal(err, null);
-                        callback();
-                    });
-            });
-        };
-
-        var findNotices = function (database, callback) {
-            database.getDb(function (err, db) {
-                assert.equal(err, null);
-                db.notices.find().sort({ _id: -1 }).toArray(function (err, result) {
-                    var transformedResult = result.map(function (obj) {
-                        var tObj = {};
-                        tObj._id = obj._id;
-                        tObj.body = obj.body;
-                        tObj.createdAt = dateFormat(mongodb.ObjectId(obj._id).getTimestamp(), "ddd, d mmm yyyy, h:MM TT");
-                        return tObj
-                    });
-                    callback(transformedResult);
-                });
-            });
-        };
     };
 
 })(module.exports);
--- data/database.js
+++ data/database.js
@@ -1,6 +1,8 @@
 (function (database) {
 
-    var mongodb = require("mongodb");
+    var assert = require('assert');
+    var dateFormat = require('dateformat');
+    var mongodb = require('mongodb');
     var mongoUrl = 'mongodb://localhost:27017/notice-board';
     var theDb = null;
 
@@ -8,7 +10,7 @@
     // Keep database connection open and return the same database 
     // connection to subsequent callers to enable connection pooling 
     // optimisations.
-    database.getDb = function (next) {
+    var getDb = function (next) {
         if (theDb) {
             next(null, theDb);
         } else {
@@ -25,5 +27,34 @@
             });
         }
     }
+    
+    // TODO: Provide an err parameter for when insert fails.
+    database.insertNotice = function (notice, callback) {
+        getDb(function (err, db) {
+            db.notices.insertOne(
+                notice,
+                function (err, result) {
+                    assert.equal(err, null);
+                    callback();
+                });
+        });
+    };
+
+    // TODO: Provide an err parameter for when find fails.
+    database.findNotices = function (callback) {
+        getDb(function (err, db) {
+            assert.equal(err, null);
+            db.notices.find().sort({ _id: -1 }).toArray(function (err, result) {
+                var transformedResult = result.map(function (obj) {
+                    var tObj = {};
+                    tObj._id = obj._id;
+                    tObj.body = obj.body;
+                    tObj.createdAt = dateFormat(mongodb.ObjectId(obj._id).getTimestamp(), "ddd, d mmm yyyy, h:MM TT");
+                    return tObj
+                });
+                callback(transformedResult);
+            });
+        });
+    };
 
 })(module.exports);
